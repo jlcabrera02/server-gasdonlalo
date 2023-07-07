8b770e39737c37fa39d3a6c851d488afdd328e7d
Jose Luis Cabrera Aguirre <aj3amdua@gmail.com>
2023-07-07 07:25:31 -0600

    Avances view directorys

--- services/viewDirectory.js
+++ services/viewDirectory.js
@@ -3,30 +3,36 @@ import fs from "fs";
 
 function scanDirs(directorio) {
   const data = [];
-  try {
-    const ls = fs.readdirSync(directorio);
-    for (let i = 0; i < ls.length; i++) {
-      const file = path.join(directorio, ls[i]);
-      let datafile = null;
-      try {
-        datafile = fs.lstatSync(file);
-      } catch (err) {}
-      if (datafile) {
-        data.push({
-          path: file,
-          isDirectory: datafile.isDirectory(),
-          length: datafile.size,
-        });
-      }
-      if (datafile.isDirectory()) {
-        scanDirs(file);
-      }
+  const ls = fs.readdirSync(directorio);
+  for (let i = 0; i < ls.length; i++) {
+    const file = path.join(directorio, ls[i]);
+    let datafile = null;
+    try {
+      datafile = fs.lstatSync(file);
+    } catch (err) {}
+    if (datafile) {
+      data.push({
+        path: file,
+        isDirectory: datafile.isDirectory(),
+        length: datafile.size,
+      });
     }
-    return data;
-  } catch (err) {}
+    if (datafile.isDirectory()) {
+      scanDirs(file);
+    }
+  }
+  return data;
 }
 
 export function documentoPreliquidaciones(req, res) {
-  const directorios = scanDirs(path.join(__dirname, "../documentos"));
-  res.status(200).json(directorios);
+  try {
+    const { pathClient } = req.query;
+    const directorios = scanDirs(
+      path.join(__dirname, "../documentos", pathClient || "")
+    );
+    res.status(200).json({ success: true, directorios });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ ...err, msg: "Directorio no encontrado" });
+  }
 }
