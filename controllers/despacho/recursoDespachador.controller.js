0815f7fadccb93092c0a6109dbbd01869600aaec
Jose Luis Cabrera Aguirre <aj3amdua@gmail.com>
2023-12-29 14:08:53 -0600

    Actualizacion de peticiones recurso despachador

--- controllers/despacho/recursoDespachador.controller.js
+++ controllers/despacho/recursoDespachador.controller.js
@@ -1,3 +1,5 @@
+import { Op } from "sequelize";
+import sequelize from "../../config/configdb";
 import models from "../../models/index";
 const { RecursosDespachadorEv, RecursosDespachador, Auditoria, empleados } =
   models;
@@ -19,6 +21,21 @@ controller.getRecursos = async (req, res) => {
 
 controller.getEv = async (req, res) => {
   try {
+    const { month, year, quincena, idEmpleado } = req.query;
+    const filtrosEv = {};
+    const filtrosEmpleado = { iddepartamento: 1 };
+
+    if (month && year) {
+      filtrosEv[Op.and] = [
+        sequelize.where(sequelize.fn("MONTH", sequelize.col("fecha")), month),
+        sequelize.where(sequelize.fn("year", sequelize.col("fecha")), year),
+      ];
+    }
+
+    if (idEmpleado) {
+      filtrosEmpleado.idempleado = idEmpleado;
+    }
+
     const response = await empleados.findAll({
       attributes: [
         "idempleado",
@@ -26,20 +43,45 @@ controller.getEv = async (req, res) => {
         "nombre",
         "apellido_paterno",
         "apellido_materno",
+        "nombre_completo",
       ],
-      where: { idempleado: 131 },
+      where: filtrosEmpleado,
       include: [
         {
           model: RecursosDespachadorEv,
-          attributes: ["idevaluacion", "evaluacion", "idrecurso", "idempleado"],
+          attributes: [
+            "idevaluacion",
+            "evaluacion",
+            "idrecurso",
+            "idempleado",
+            "quincena",
+            "fecha",
+            "createdAt",
+          ],
           as: "evaluaciones",
           include: RecursosDespachador,
+          where: filtrosEv,
         },
       ],
     });
 
-    return res.status(200).json({ succcess: true, response });
+    if (quincena) {
+      const newResponse = JSON.parse(JSON.stringify(response)).map((emp) => ({
+        ...emp,
+        evaluaciones: emp.evaluaciones.filter(
+          (ev) => ev.quincena === Number(quincena)
+        ),
+      }));
+
+      return res.status(200).json({
+        success: true,
+        response: newResponse,
+      });
+    }
+
+    return res.status(200).json({ success: true, response });
   } catch (err) {
+    console.log(err);
     res.status(400).json({ success: false, err });
   }
 };
@@ -55,6 +97,73 @@ controller.createEv = async (req, res) => {
     }));
 
     const response = await RecursosDespachadorEv.bulkCreate(ev);
+
+    // await Auditoria.create({});
+
+    return res.status(200).json({ succcess: true, response });
+  } catch (err) {
+    res.status(400).json({ success: false, err });
+  }
+};
+
+controller.updateEv = async (req, res) => {
+  try {
+    const { evaluaciones, fecha } = req.body;
+    const ev = evaluaciones.map((ev) => ({
+      evaluacion: ev.evaluacion,
+      idevaluacion: ev.idEvaluacion,
+      idempleado: ev.idEmpleado,
+      idrecurso: ev.idRecurso,
+      fecha,
+    }));
+
+    const response = await sequelize.transaction(async (t) => {
+      const responses = [];
+      for (let i = 0; i < ev.length; i++) {
+        const update = await RecursosDespachadorEv.update(
+          { evaluacion: ev[i].evaluacion, fecha: ev[i].fecha },
+          { where: { idevaluacion: ev[i].idevaluacion }, transaction: t }
+        );
+
+        responses.push({
+          status: update,
+          idrecurso: ev[i].idrecurso,
+          idevaluacion: ev[i].idevaluacion,
+        });
+      }
+
+      return responses;
+    });
+
+    // await Auditoria.create({});
+
+    return res.status(200).json({ succcess: true, response });
+  } catch (err) {
+    res.status(400).json({ success: false, err });
+  }
+};
+
+controller.deleteEv = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const ids = id.map((id) => Number(id));
+
+    const response = await sequelize.transaction(async (t) => {
+      const responses = [];
+      for (let i = 0; i < ids.length; i++) {
+        const update = await RecursosDespachadorEv.destroy({
+          where: { idevaluacion: ids[i] },
+          transaction: t,
+        });
+
+        responses.push({
+          status: update,
+          idevaluacion: ids[i],
+        });
+      }
+
+      return responses;
+    });
 
     // await Auditoria.create({});
 
--- routes/d.recursosDespachador.router.js
+++ routes/d.recursosDespachador.router.js
@@ -6,5 +6,7 @@ const route = router();
 route.get("/obtener-evaluaciones", rd.getEv);
 route.get("/obtener-recursos", rd.getRecursos);
 route.post("/evaluacion", rd.createEv);
+route.put("/actualizar-evaluacion", rd.updateEv);
+route.delete("/eliminar-evaluacion", rd.deleteEv);
 
 export default route;
