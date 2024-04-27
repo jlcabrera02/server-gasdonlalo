fc11d9d2b856972cc1c3554000fd6897334273d4
Jose Luis Cabrera Aguirre <aj3amdua@gmail.com>
2024-04-27 09:13:30 -0600

    Peticiones PM administrativo

--- controllers/despacho/configuraciones.controller.js
+++ controllers/despacho/configuraciones.controller.js
@@ -2,7 +2,9 @@ import {
   obtenerConfiguraciones,
   escribirConfiguraciones,
 } from "../../services/configuracionesPersonalizables";
+import model from "../../models";
 const controller = {};
+const { PM } = model;
 
 controller.getConfig = async (req, res) => {
   try {
@@ -26,6 +28,43 @@ controller.setConfig = async (req, res) => {
     });
 
     return res.status(200).json({ success: true, response: configuraciones });
+  } catch (err) {
+    res.status(400).json({ success: false, err });
+  }
+};
+
+controller.getPM = async (req, res) => {
+  try {
+    const response = await PM.findAll({
+      order: [
+        ["fecha", "ASC"],
+        ["createdAt", "ASC"],
+      ],
+    });
+
+    return res.status(200).json({ success: true, response });
+  } catch (err) {
+    res.status(400).json({ success: false, err });
+  }
+};
+
+controller.createPM = async (req, res) => {
+  try {
+    const { evaluacion, cantidad, fecha } = req.body;
+    const response = await PM.create({ evaluacion, cantidad, fecha });
+
+    return res.status(200).json({ success: true, response });
+  } catch (err) {
+    res.status(400).json({ success: false, err });
+  }
+};
+
+controller.deletePM = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await PM.destroy({ where: { idpuntajes_minimo: id } });
+
+    return res.status(200).json({ success: true, response });
   } catch (err) {
     res.status(400).json({ success: false, err });
   }
--- routes/d.configDespacho.js
+++ routes/d.configDespacho.js
@@ -1,7 +1,12 @@
 import router from "express-promise-router";
 import configDesp from "../controllers/despacho/configuraciones.controller";
+import controller from "../controllers/despacho/configuraciones.controller";
 
 const route = router();
+
+route.get("/pm-configuraciones", controller.getPM);
+route.post("/pm-configuraciones", controller.createPM);
+route.delete("/pm-configuraciones/:id", controller.deletePM);
 
 route.get("/configuraciones", configDesp.getConfig);
 route.put("/configuraciones/editar", configDesp.setConfig);
